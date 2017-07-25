5e422e58396b8754d9aaa054882ea88d73f07df3
Matt Pitts <mattpittsmusic@gmail.com>
2017-07-25 10:53:06 -0600

    blah blah blah

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgresql://localhost/warmup'
-  }
+},
+production: {
+client: 'pg',
+connection: process.env.DATABASE_URL
+}
 
 };
